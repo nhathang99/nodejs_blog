f3ac8308bfa966daf847ae46bc8649a85bfe8416
nhathang99 <17520437@gm.uit.edu.vn>
2022-05-30 14:17:19 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/tin-tuc', (req, res) => {
+  res.send('Hello World!')
+})
+var a =1;
+var b =2;
+var c = a+b;
+//127.0.0.1-localhost 
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
