3bcf22ffab3c6bff27dffba85e936664a5c1cb73
nhathang99 <17520437@gm.uit.edu.vn>
2022-05-30 15:15:07 +0700

    add handlebars

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
+const { engine } = require("express-handlebars");
+const app = express();
+
+const port = 3000;
+//HTTP logger
+app.use(morgan("combined"));
+
+//template engine
+
+app.engine(".hbs", engine({ extname: ".hbs" }));
+app.set("view engine", ".hbs");
+app.set("views", path.join(__dirname, "resources/views"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/new", (req, res) => {
+  res.render("new");
+});
+
+//127.0.0.1-localhost
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
